04ff327c1ac63256c59d947c0c1905c6caa342e2
jodevera <jodevera@deloitte.com>
2022-09-06 13:25:05 +0800

    fixed loader bugs

--- components/metadata/metadata.component.tsx
+++ components/metadata/metadata.component.tsx
@@ -9,7 +9,14 @@ import { MetadataComponentProps } from "../../types/MasterPageComponent.type";
 import { Metadata } from "../../types/MasterTypes.types";
 
 import LoaderComponent from "../loader/loader.component";
-import { PlusIcon, XIcon, PencilIcon, TrashIcon } from "@heroicons/react/solid";
+import { Typography, List, ListItem, ListItemButton, ListItemText, IconButton, Container, Box, Card, CardContent, CardActions } from "@mui/material";
+import { spacing } from '@mui/system';
+import DeleteIcon from '@mui/icons-material/Delete';
+import CreateIcon from '@mui/icons-material/Create';
+import AddIcon from '@mui/icons-material/Add';
+import CloseIcon from '@mui/icons-material/Close';
+import { ClassNames } from "@emotion/react";
+
 
 const MetadataComponent: FC<MetadataComponentProps> = ({
   type,
@@ -17,26 +24,26 @@ const MetadataComponent: FC<MetadataComponentProps> = ({
   onMetadataClick,
   enableRowActions,
 }: MetadataComponentProps) => {
-  const tailwindClasses = {
-    container:
-      "relative flex-grow flex flex-col bg-white p-1 min-h-[200px] md:min-h-100 md:w-[47vw] lg:w-[27vw] border-[1px] shadow-lg",
-    toolbar: "flex flex-row",
-    title: "flex-1",
-    submitButton:
-      "h-iconbutton w-iconbutton flex items-center justify-center p-0",
-    list: "list flex-grow flex flex-col overflow-auto max-h-[300px] md:max-h-unset",
-    lineItem:
-      "lineitem transition-all duration-500 rounded py-1 px-2 flex flex-row",
-    lineItemActive: "active bg-sidebar text-white min-h-0",
-    lineDetails:
-      "name flex flex-col justify-start justify-center flex-grow cursor-pointer",
-    lineActions: "lineActions flex flex-row justify-center items-center",
-    lineButton:
-      "lineButton h-[20px] w-[20px] cursor-pointer hover:text-current",
-    icon: "h-5 w-5 text-gray",
-    description: "block w-full text-xs",
-    name: "p-0 m-0",
-  };
+  // const tailwindClasses = {
+  //   container:
+  //     "relative flex-grow flex flex-col bg-white p-1 min-h-[200px] md:min-h-100 md:w-[47vw] lg:w-[27vw] border-[1px] shadow-lg",
+  //   toolbar: "flex flex-row",
+  //   title: "flex-1",
+  //   submitButton:
+  //     "h-iconbutton w-iconbutton flex items-center justify-center p-0",
+  //   list: "list flex-grow flex flex-col overflow-auto max-h-[300px] md:max-h-unset",
+  //   lineItem:
+  //     "lineitem transition-all duration-500 rounded py-1 px-2 flex flex-row",
+  //   lineItemActive: "active bg-sidebar text-white min-h-0",
+  //   lineDetails:
+  //     "name flex flex-col justify-start justify-center flex-grow cursor-pointer",
+  //   lineActions: "lineActions flex flex-row justify-center items-center",
+  //   lineButton:
+  //     "lineButton h-[20px] w-[20px] cursor-pointer hover:text-current",
+  //   icon: "h-5 w-5 text-gray",
+  //   description: "block w-full text-xs",
+  //   name: "p-0 m-0",
+  // };
 
   //state hook to capture api response to SkillType array
   const [metadataList, setMetadataList] = useState<Metadata[]>([]);
@@ -88,7 +95,24 @@ const MetadataComponent: FC<MetadataComponentProps> = ({
     renderData();
   }, [renderData]);
 
-  const handleFormDisplay = () => {
+  const metadataTitle = () => {
+    let title: string;
+    switch (type) {
+      case "skill":
+        title = "Skills";
+        return title;
+      case "capability":
+        title = "Capabilities";
+        return title;
+      case "industry":
+        title = "Industries";
+        return title;
+      default:
+        break;
+    }
+  };
+
+  const cardBody = () => {
     if (displayForm) {
       return (
         <MetadataForm
@@ -107,91 +131,62 @@ const MetadataComponent: FC<MetadataComponentProps> = ({
       );
     } else {
       return (
-        <div className={tailwindClasses.list}>
+        <List>
           {metadataList.map((metadata) => {
             const activeLine = activeMetadata?._id === metadata._id;
             return (
-              <div
-                key={`${type}-line-item-${metadata._id}`}
-                className={`${tailwindClasses.lineItem} ${
-                  activeLine ? tailwindClasses.lineItemActive : ""
-                }`}
-              >
-                <div
-                  className={`${tailwindClasses.lineDetails}`}
-                  onClick={() => {
-                    clickMetadataRow(metadata);
-                  }}
-                >
-                  <p className={tailwindClasses.name}>
-                    <span>{metadata.name}</span>
-                  </p>
-                  <span className={tailwindClasses.description}>
-                    {activeLine ? metadata.description : ""}
-                  </span>
-                </div>
-                {enableRowActions ? (
-                  <div className={tailwindClasses.lineActions}>
-                    <PencilIcon
-                      className={tailwindClasses.lineButton}
-                      onClick={() => {
-                        editMetadata(metadata);
-                      }}
-                    />
-                    <TrashIcon
-                      className={tailwindClasses.lineButton}
-                      onClick={() => {
+              <ListItem key={`${type}-line-item-${metadata._id}`} component="div" disablePadding
+                secondaryAction={enableRowActions ? (
+                  <>
+                    <IconButton>
+                      <CreateIcon
+                        onClick={() => {
+                          editMetadata(metadata);
+                        }}
+                      />
+                    </IconButton>
+                    <IconButton>
+                      <DeleteIcon onClick={() => {
                         removeMetadata(metadata);
-                      }}
-                    />
-                  </div>
-                ) : null}
-              </div>
+                      }} />
+                    </IconButton>
+                  </>
+                ) : null
+                }>
+                <ListItemButton>
+                  <ListItemText primary={metadata.name} />
+                </ListItemButton>
+                <span>
+                  {activeLine ? metadata.description : ""}
+                </span>
+              </ListItem>
             );
           })}
-        </div>
+        </List>
       );
     }
   };
 
-  const metadataTitle = () => {
-    let title: string;
-    switch (type) {
-      case "skill":
-        title = "Skills";
-        return title;
-      case "capability":
-        title = "Capabilities";
-        return title;
-      case "industry":
-        title = "Industries";
-        return title;
-      default:
-        break;
-    }
-  };
-
   return (
-    <div className={tailwindClasses.container}>
-      <div className={tailwindClasses.toolbar}>
-        <p className={tailwindClasses.title}>{metadataTitle()}</p>
-        {displayPopup ? (
-          <p></p>
-        ) : (
-          <button
-            className={tailwindClasses.submitButton}
-            onClick={showMetadataForm}
-          >
-            {displayForm ? (
-              <XIcon className={tailwindClasses.icon} />
-            ) : (
-              <PlusIcon className={tailwindClasses.icon} />
-            )}
-          </button>
-        )}
-      </div>
-      {loadState ? <LoaderComponent /> : handleFormDisplay()}
-    </div>
+    <Container sx={{ px: 1 }} disableGutters>
+      <Card variant="outlined">
+        {loadState? <LoaderComponent/> : <>
+        <CardActions sx={{ p: 1 }}>
+            <Typography variant="h6" component="div">{metadataTitle()}</Typography>
+            <IconButton onClick={showMetadataForm}>
+              {displayForm ? (
+                <CloseIcon />
+              ) : (
+                <AddIcon />
+              )}
+            </IconButton>
+          </CardActions>
+          <CardContent sx={{ p: 0 }}>
+            {cardBody()}
+          </CardContent>
+        </>}
+      </Card>
+    </Container>
   );
 };
 
