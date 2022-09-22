99c4a279078dbdf1d25844e69919717e67cc2e8a
jodevera <jodevera@deloitte.com>
2022-09-22 13:09:24 +0800

    fixed responsiveness bug

--- pages/metadatas.tsx
+++ pages/metadatas.tsx
@@ -1,35 +1,17 @@
 import { useState, useEffect } from 'react';
-
-import PageBanner from '../components/pageBanner/pageBanner.component';
 import MetadataComponent from '../components/metadata/metadata.component';
 
-import { Container, Box, Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 
 import { MetadataType } from '../types/MasterTypes.types';
 import ButtonComponent from '../components/ButtonComponent';
 
 const MetadataPage = () => {
-  const tailwindClasses = {
-    customBanner: 'h-full w-full flex items-center justify-start p-[2rem]',
-    content:
-      'flex relative md:mx-0 md:mr-4 md:pl-[1rem] pt-1 md:py-[1rem] flex flex-col md:flex-row gap-1 md:gap-[1rem] z-[1] md:flex-grow h-full mb-1',
-    header: 'text-2xl text-white z-[5]',
-    box: 'flex flex-grow overflow-auto ',
-    mobileBoxShow: 'flex',
-    mobileBoxHidden: 'md:flex hidden',
-    mobileTab: 'md:hidden flex justify-center',
-    mobileTabOption: '',
-    formButton:
-      'm-[10px] font-semibold hover:text-white py-2 px-4 border border-sidebar hover:border-transparent rounded',
-    formButtonActive: 'bg-sidebar text-white',
-    formButtonInactive:
-      'bg-transparent text-white hover:bg-sidebar text-sidebar',
-  };
   const [activeSkill, setActiveSkill] = useState<MetadataType>();
   const [activeCapability, setActiveCapability] = useState<MetadataType>();
   const [activeIndustry, setActiveIndustry] = useState<MetadataType>();
   const [activeMobileTab, setActiveMobileTab] = useState<string>('Skills');
-  const [isMobile, setIsMobile] = useState<boolean>();
+  const [isMobile, setIsMobile] = useState<boolean>(false);
   const mobileTabs = ['Skills', 'Capabilities', 'Industries'];
 
   const onMobileTabClick = (tab: string) => {
@@ -49,33 +31,14 @@ const MetadataPage = () => {
   }
 
   useEffect(() => {
-    window.addEventListener("resize", handleResize)
+    window.addEventListener("resize", handleResize);
+    handleResize();
   })
 
 
 
   return (
     <>
-      {/* <PageBanner
-        content={
-          <Container
-            sx={{
-              p: 0,
-              height: '15vh',
-              width: '100%',
-              display: 'flex',
-              alignItems: 'center',
-            }}
-            maxWidth={false}
-          >
-            <Typography sx={{ zIndex: '5' }} variant='h4' component='h4'>
-              Metadata List
-            </Typography>
-          </Container>
-        }
-      /> */}
-
-
       <Box
         sx={{
           pt: { xs: 1, md: 0 },
