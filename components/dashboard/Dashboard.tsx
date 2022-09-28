c8d39aa98838bf1a3c3e7ed41f06d07d9b858a50
wpnenriquez <wenriquez@deloitte.com>
2022-09-28 10:53:32 +0800

    created graphs card; fixed issue with dashboard icons floating when scrolling

--- components/dashboard/Dashboard.tsx
+++ components/dashboard/Dashboard.tsx
@@ -21,10 +21,10 @@ const Dashboard: FC<DashboardType> = ({ cards }) => {
             key={index}
             className='text-black w-44 h-40 md:w-48 md:h-44 bg-white rounded-3xl'
           >
-            <div className='bg-main rounded-2xl px-6 py-2 w-10 absolute -mt-5 ml-2 text-white flex justify-center'>
+            <div className='bg-main rounded-2xl px-6 py-2 w-10 -mt-5 ml-2 text-white flex justify-center'>
               {item.icon}
             </div>
-            <div className='w-full h-full p-6 flex flex-col items-center justify-center'>
+            <div className='w-full p-6 flex flex-col items-center justify-center'>
               <div className='text-center font-semibold text-xs'>
                 TOTAL NUMBER OF {item.title}
               </div>
