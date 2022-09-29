f66ef61533b70e7e30fdf6f6176bf3d57023b68b
jodevera <jodevera@deloitte.com>
2022-09-29 13:13:27 +0800

    merged with develop

--- components/layout/layout.component.tsx
+++ components/layout/layout.component.tsx
@@ -13,40 +13,8 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div>
       <SidebarComponent />
-<<<<<<< HEAD
-      <Container
-        sx={{
-          pt: {
-            xs: '140px',
-            sm: '185px',
-          },
-          display: 'flex',
-          flexDirection: 'column',
-          maxHeight: '100%',
-          overflow: 'auto'
-        }}
-        maxWidth={false}
-        disableGutters
-      >
-        {children}
-      </Container>
-    </Box>
-    // <div className={tailwindClasses.layout}>
-    //   <HeaderBar
-    //     breadcrumb={breadcrumb}
-    //     onMenuClick={() => setSidebarShow(!sidebarShow)}
-    //     show={sidebarShow}
-    //   />
-    //   <div className={tailwindClasses.body}>
-    //     {/* <Sidebar onRouteClick={goToRoute} routes={AppRoutes} show={sidebarShow} /> */}
-    //     <SidebarComponent />
-    //     <div className={`${tailwindClasses.content}`}>{children}</div>
-    //   </div>
-    // </div>
-=======
       <div className='sm:ml-20 sm:mr-4'>{children}</div>
     </div>
->>>>>>> c9a67cea519ff251836591134157a0b3f16505c0
   );
 }
 
