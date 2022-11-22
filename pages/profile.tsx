5c6af152ad9c9f61270f1fc3c9003bf1d7d4cec7
arnoldcostimianoV <arnoldverzo0@gmail.com>
2022-11-22 10:59:49 +0800

    added profile features design

---
+++ pages/profile.tsx
@@ -0,0 +1,13 @@
+import Profile from '../components/profile/profile.components';
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <div className=' '>
+        <Profile />
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
